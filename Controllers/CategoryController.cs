a6683f09c27f6e0a1ca11a9ba7f94eea2618963c
Akash Shettigar <akashshettigar97@gmail.com>
2022-08-03 17:13:38 +0530

    crud operation

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,126 @@
+﻿using BulkybookWeb.Data;
+using BulkybookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkybookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList= _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Display order cannot exact match the Name");
+            }
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category Created Successfully..!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+            
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Categories.Find(id);
+
+           // var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+           // var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Display order cannot exact match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category Updated Successfully..!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Categories.Find(id);
+
+            // var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            // var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+           
+             _db.Categories.Remove(obj);
+               _db.SaveChanges();
+            TempData["Success"] = "Category Deleted Successfully..!";
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkybookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkybookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)  //configuring db context
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
